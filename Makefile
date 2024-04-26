da6b5aa74b13ab35198de95b05344dc67f5b26dd
JivusAyrus <suvijsurya76@gmail.com>
2024-04-26 16:35:38 +0530

    feat: improve

--- Makefile
+++ Makefile
@@ -5,10 +5,10 @@ run-apollo-router:
 	./scripts/run-apollo-router.sh
 
 run-apollo-gateway:
-	cd apollo-gateway && npm run start
+	cd apollo-gateway && npm i && npm run start
 
 run-webhook-app:
-	cd webhook-app && npm run dev
+	cd webhook-app && npm i && npm run dev
 
 run-subgraphs:
 	./scripts/run-subgraphs.sh
--- scripts/run-subgraphs.sh
+++ scripts/run-subgraphs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 npx concurrently --kill-others \
-    "cd subgraphs/inventory && npm run start" \
-    "cd subgraphs/pandas && npm run start" \
-    "cd subgraphs/products && npm run start" \
-    "cd subgraphs/users && npm run start"
+    "cd subgraphs/inventory && npm i && npm run start" \
+    "cd subgraphs/pandas && npm i && npm run start" \
+    "cd subgraphs/products && npm i && npm run start" \
+    "cd subgraphs/users && npm i && npm run start"
